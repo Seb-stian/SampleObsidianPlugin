f07388a4e46c31641d22740a019572706e6cc136
Sebastián <sebastian.svobo@gmail.com>
2020-10-18 22:27:26 +0200

    Initial commit

---
+++ SampleObsidianPlugin/SampleObsidianPlugin/SamplePlugin.cs
@@ -0,0 +1,16 @@
+﻿using Obsidian.Plugins.API;
+using Obsidian.Plugins.API.Services;
+
+namespace SampleObsidianPlugin
+{
+    [Plugin(Authors = "Seb-stian", Description = "Sample plugin used for remote plugin testing.", Name = "Sample Remote Plugin", ProjectUrl = "https://github.com/Seb-stian/SampleObsidianPlugin", Version = "1.0")]
+    public class SamplePlugin : PluginBase
+    {
+        [Inject] public ILogger Logger { get; set; }
+
+        public void OnLoad()
+        {
+            Logger.Log("Hello from github.com/Seb-stian/SampleObsidianPlugin!");
+        }
+    }
+}
