9ecd7ed9a2d4e40b7cd1330205e362ddbe1e5eb9
Sebastián <sebastian.svobo@gmail.com>
2020-10-25 17:42:45 +0100

    Renamed plugin

--- SampleObsidianPlugin/SampleObsidianPlugin/SampleRemotePlugin.cs
+++ SampleObsidianPlugin/SampleObsidianPlugin/SampleRemotePlugin.cs
@@ -6,7 +6,7 @@ namespace SampleObsidianPlugin
     [Plugin(Name = "Sample Remote Plugin", Authors = "Seb-stian", Version = "1.0",
         Description = "Sample plugin used for remote plugin testing.",
         ProjectUrl = "https://github.com/Seb-stian/SampleObsidianPlugin")]
-    public class SamplePlugin : PluginBase
+    public class SampleRemotePlugin : PluginBase
     {
         [Inject] public ILogger Logger { get; set; }
 
